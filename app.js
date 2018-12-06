c311df7e62b92a3d989974eddcd234922234486a
Raju Gangadhar <RajuSuranagi@users.noreply.github.com>
2018-12-06 23:44:23 +0530

    Added app.js file

---
+++ app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ProgressBar from 'progressBar';
+
+ReactDOM.render(
+  <Progressbar percentage={25} />,
+  document.getElementById('content'),
+);
